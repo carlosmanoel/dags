b88eb89b7a1e2f6112306aaa5343a7c18e97f366
Carlos Manoel de Oliveira <carlos.manoel@gmail.com>
2021-06-23 22:31:15 -0300

    first dag

---
+++ first-dag.py
@@ -0,0 +1,37 @@
+from datetime import date, timedelta
+
+from airflow.models import DAG
+
+from airflow.models import Variable
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'DataLab',
+    'start_date': days_ago(2),
+    'catchup': False
+}
+
+DAG_PREFIX = "minha_dag"
+DAG_VERSION = "1.0"
+
+with DAG(
+        dag_id=DAG_PREFIX + "_" + DAG_VERSION,
+        default_args=default_args,
+        schedule_interval="@hourly",
+        tags=['example']
+) as dag:
+
+    sleep_command = BashOperator(
+        task_id='sleep_command',
+        bash_command='sleep 5 ',
+        dag=dag,
+    )
+
+    sleep_again = BashOperator(
+        task_id='sleep_again',
+        bash_command='sleep 5 ',
+        dag=dag,
+    )
+
+    sleep_command >> sleep_again
