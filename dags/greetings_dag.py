0a26ab3545e90c1746f38c1a1cc456b9ba496882
Carlos Manoel de Oliveira <carlos.manoel@gmail.com>
2021-07-08 22:45:28 -0300

    operators

--- dags/greetings_dag.py
+++ dags/greetings_dag.py
@@ -3,7 +3,7 @@ from typing import Dict
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
 
-from datalab import HelloOperator
+from datalab.operators.hellooperator import HelloOperator
 
 _DEFAULT_ARGS = {"owner": "datalab"}
 
